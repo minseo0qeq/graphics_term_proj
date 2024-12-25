3d0dfc9c722796dee6fdb6ac938c79520a9ae25b
hyeongchanan <84336265+hyeongchanan@users.noreply.github.com>
2024-12-25 18:09:22 +0900

    타이틀
    타이틀

--- Assets/UI/script/SceneMove.cs
+++ Assets/UI/script/SceneMove.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class moveScean : MonoBehaviour
 {
-    public void LoadMain()
+    public void LoadFFKSampleScene()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("FFK Sample Scene");
     }
 
     public void LoadTitle()
