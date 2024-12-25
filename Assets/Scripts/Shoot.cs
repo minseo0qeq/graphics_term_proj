3f368f05b502151f018ea7877b66095c83ecf494
duckbr0ther <duckbro@naver.com>
2024-12-25 15:05:28 +0900

    shoot commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private Vector3 localScale = new Vector3(0.2f, 0.2f, 0.2f);
+    [SerializeField] private float forceAmount = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            SpawnBullet();
+        }
+    }
+
+    void SpawnBullet()
+    {
+
+        GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        bullet.transform.position = transform.position;
+        bullet.transform.localScale = localScale;
+        
+        Bullet bulletScript = bullet.AddComponent<Bullet>(); 
+        bulletScript.SetSpeed(bulletSpeed);
+        bulletScript.SetDirection(transform.forward);
+        bulletScript.SetForceAmount(forceAmount);
+
+        SphereCollider sc = bullet.AddComponent<SphereCollider>();
+        sc.isTrigger = true;
+        sc.radius = 0.2f;
+
+        Destroy(bullet, 10f);
+    }
+}
+
+public class Bullet : MonoBehaviour
+{
+    private float speed;
+    private float forceAmount;
+    private Vector3 direction;
+
+    // Set the speed of the bullet
+    public void SetSpeed(float bulletSpeed)
+    {
+        speed = bulletSpeed;
+    }
+
+    public void SetDirection(Vector3 dir){
+        direction = dir;
+    }
+
+    public void SetForceAmount(float setForceAmount){
+        forceAmount = setForceAmount;
+    }
+
+    void Update()
+    {
+        // Move the bullet forward over time
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider collider){
+        GameObject hitObject = collider.gameObject;
+        string hitObjectName = hitObject.name;
+        string hitObjectTag = hitObject.tag;
+        Debug.Log("Hit Object: " + hitObjectName);
+
+        if(hitObjectTag == "Target"){
+            Rigidbody hitObjectRb = hitObject.GetComponent<Rigidbody>();
+
+            if(hitObjectRb == null){
+                Debug.LogError("No RigidBody on Target");
+                return;
+            }
+
+            hitObjectRb.useGravity = true;
+            hitObjectRb.AddForce(direction * forceAmount);
+
+            Destroy(gameObject);
+        }
+    }
+}
+
